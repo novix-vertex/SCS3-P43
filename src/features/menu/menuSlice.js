d2456ad325d6324728b03310f0252491c3482675
chirag jain <jchirag0610@gmail.com>
2026-07-26 22:27:29 +0530

    - menu api calling using axios inside async thunk of redux (no direct api call by react), as it is asynchronous task so added in extrareducers instead of reducers of menu slice - added all three state (pending, fullfilled and rejected)

--- src/features/menu/menuSlice.js
+++ src/features/menu/menuSlice.js
@@ -1,14 +1,40 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import api from "../../api/axios";
 
 const initialState = {
-    menu:[]
+    menu: [],
+    loading: false,
+    error:null
 };
 
-const menuSlice = createSlice({
-    name:"menu",
+export const menuSlice = createSlice({
+    name: "menu",
     initialState,
-    reducers:{}
+    reducers: {},
+    extraReducers: (builder) => {
+        builder.addCase(fetchMenu.pending, (state) => {
+            state.loading = true;
+        });
+
+        builder.addCase(fetchMenu.fulfilled, (state, action) => {
+            state.loading = false;
+            state.menu = action.payload;
+        });
+
+        builder.addCase(fetchMenu.rejected, (state, action) => {
+            state.loading = false;
+            state.error = action.error.message;
+        });
+    }
 });
+
+export const fetchMenu = createAsyncThunk(
+    "menu/fetchMenu",
+    async () => {
+        const res = await api.get("/menu");
+        return res.data;
+    }
+);
 
 
 export default menuSlice.reducer;
