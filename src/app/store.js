f7cbc471aff8facafb2affbd98ab7f6b0d2a8aab
chirag jain <jchirag0610@gmail.com>
2026-07-26 21:36:42 +0530

    menu slice connected with store as a reducer

--- src/app/store.js
+++ src/app/store.js
@@ -1,3 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit" ;
+import {menuReducer} from "../features/menu/menuSlice";
 
-export const store = configureStore(); 
+export const store = configureStore({
+    reducer:{
+        menu:menuReducer
+    }
+}); 
