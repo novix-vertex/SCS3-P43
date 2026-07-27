e95ab8ab7424af3a2213707f063c4dc0dad242dc
chirag jain <jchirag0610@gmail.com>
2026-07-27 16:08:16 +0530

    menu listing empty states ui refined

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -22,7 +22,30 @@ const Home = () => {
         return <h2>Loading...</h2>
     }
     if (error) {
-        return <h2>{error}</h2>
+
+        return (
+            <div className="text-center py-20">
+                <div className="text-7xl">⚠️</div>
+
+                <h2 className="text-2xl font-bold mt-5">
+                    Something went wrong
+                </h2>
+
+                <p className="text-gray-500 mt-3">
+                    Unable to fetch menu items.
+                </p>
+
+                <button
+                    onClick={() => dispatch(fetchMenu())}
+                    className="mt-5 bg-orange-500 text-white px-6 py-2 rounded-lg"
+                >
+                    Try Again
+                </button>
+
+            </div >
+
+        )
+
     }
 
 
@@ -35,6 +58,30 @@ const Home = () => {
 
         return searchFilter && categoryFilter;
     });
+
+    if (filteredMenu.length === 0) {
+        return (
+            <div className="bg-gray-100 min-h-screen">
+                <Navbar />
+
+                <div className="min-h-100 flex flex-col items-center justify-center">
+
+                    <div className="text-5xl">🔍</div>
+
+                    <h2 className="text-2xl font-bold mt-5">
+                        No Menu Items Found
+                    </h2>
+
+                    <p className="text-gray-500 mt-3">
+                        Try another search  
+                    </p>
+
+                </div>
+
+                <CartSidebar />
+            </div>
+        );
+    }
 
     return (
         <div className="bg-gray-100 min-h-screen">
